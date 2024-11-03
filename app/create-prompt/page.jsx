1e299101be0e8cbc17d6d6bb6663da25eea90a84
hishammfaume <hishammfaume@gmail.com>
2024-11-03 12:57:26 +0000

    created the create post page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { useState } from "react"
-import {useSession} from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import Form from '@components/form';
-
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Form from "/components/form";
 
 const CreatePrompt = () => {
-const [ submitting, se]
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const createPrompt = (e) => {};
 
   return (
-      <Form />
-  )
-}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post </span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts
+      </p>
 
-export default Form
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label htmlFor="">
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI prmpt
+          </span>
+        </label>
+
+        <textarea
+          value={post.prompt}
+          onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+          placeholder="write your prompt here..."
+          required
+          className="form_textarea"
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Form;
