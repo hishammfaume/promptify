16a98923a429985317e41fc4bf92e7c76adba95c
hishammfaume <hishammfaume@gmail.com>
2024-11-05 21:56:15 +0000

    "Added PATCH and DELETE endpoints to users/[id]/posts route, and updated profile page with handleEdit and handleDelete functions"

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -18,3 +18,43 @@ export const GET = async (request, {params}) => { //params get populate if you p
         console.log(error);
     }
 }
+
+//patch update
+export const PATCH = async (request, {params}) => {
+    const {prompt, tag} = await request.json();
+
+    try{
+        await connectToDB();
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt){
+            return new Response("Prompt not found", {status: 404});
+        };
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+        return new Response(JSON.stringify(existingPrompt), {status: 200});
+
+    } catch(err){
+        return new Response("Failed to update prompt", {status: 500})
+        console.log(err);
+    }
+}
+
+
+//delete
+
+export const DELETE = async (request, {params}) => {
+    try{
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+        return new Response("Prompt deleted", {status: 200});
+        
+
+    } catch(err){
+        return new Response("Failed to delete prompt", {status: 500})
+        console.log(err);
+    }
+}
